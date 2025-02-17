33de040523398887d135b9c991d2ce0072d7d20a
mdoakil902 <159051140+Mdoakil@users.noreply.github.com>
2025-02-17 19:57:15 +0600

    stats and navigation primary structure done

--- app/src/main/java/com/oakil/incomeandexpensetracker/NavHostScreen.kt
+++ app/src/main/java/com/oakil/incomeandexpensetracker/NavHostScreen.kt
@@ -1,12 +1,20 @@
 package com.oakil.incomeandexpensetracker
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.NavigationBarItemColors
 import androidx.compose.material3.NavigationBarItemDefaults
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -23,16 +31,42 @@ import kotlinx.coroutines.selects.select
 @Composable
 fun NavHostScreen() {
     val navController = rememberNavController()
+    var bottomBarVisibility by remember {
+        mutableStateOf(true)
+    }
 
-    NavHost(navController = navController, startDestination = "/home") {
-        composable(route = "/home") {
-            HomeScreen(navController)
+    Scaffold(bottomBar = {
+
+        AnimatedVisibility(visible = bottomBarVisibility) {
+            NavigationBottomBar(
+                navController = navController,
+                items = listOf(
+                    NavItem(route = "/home", icon = R.drawable.ic_home),
+                    NavItem(route = "/stats", icon = R.drawable.ic_stats)
+
+                )
+            )
         }
-        composable(route = "/add") {
-            AddExpense(navController)
-        }
-        composable(route = "/stats") {
-            StatesScreen(navController)
+
+    }) {
+
+        NavHost(
+            navController = navController,
+            startDestination = "/home",
+            modifier = Modifier.padding(it)
+        ) {
+            composable(route = "/home") {
+                bottomBarVisibility = true
+                HomeScreen(navController)
+            }
+            composable(route = "/add") {
+                bottomBarVisibility = false
+                AddExpense(navController)
+            }
+            composable(route = "/stats") {
+                bottomBarVisibility = true
+                StatesScreen(navController)
+            }
         }
     }
 }
@@ -56,25 +90,31 @@ fun NavigationBottomBar(
 
     BottomAppBar {
         items.forEach { item ->
-            NavigationBarItem(selected = currentRoute == item.route, onClick = {
-                navController.navigate(item.route) {
-                    popUpTo(navController.graph.startDestinationId)
-                    launchSingleTop = true
-                    restoreState = true
-                }
-            },
+            NavigationBarItem(
+                selected = currentRoute == item.route, onClick = {
+                    navController.navigate(item.route) {
+                        popUpTo(navController.graph.startDestinationId)
+                        launchSingleTop = true
+                        restoreState = true
+                    }
+                },
                 icon = {
-                Icon(painter = painterResource(id = item.icon), contentDescription = null)
-            },
+                    Icon(painter = painterResource(id = item.icon), contentDescription = null)
+                },
                 alwaysShowLabel = false,
                 colors = NavigationBarItemDefaults.colors(
                     selectedTextColor = Zinc,
-                    selectedIconColor = Zinc
-                )
-                )
+                    selectedIconColor = Zinc,
+                    unselectedIconColor = Color.Gray,
+                    unselectedTextColor = Zinc,
+
+
+                    )
+            )
         }
 
     }
 
 }
+
 
